6b40b16486a8d76b9b3aa0df22e285c1b1486e27
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-04 16:07:25 +1300

    added funcs that update name and day state with form input on change

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -3,17 +3,20 @@ import React from 'react';
 
 const Landing = ({name, setName, day, setDay}) => {
 
-
-
     // Handle form submitting
     const handleSubmit = (e) => {
         e.preventDefault();
         e.target.reset()
     }
 
-    // Handles updating state with input values 
-    const handleUpdate = (e) => {
+    // Handles updating name State with input value 
+    const handleNameUpdate = (e) => {
+        setName(e.target.value)
+    }
 
+    // Handles updating Day State with input value 
+    const handleDayUpdate = (e) => {
+        setDay(e.target.value)
     }
 
     return(
@@ -21,8 +24,8 @@ const Landing = ({name, setName, day, setDay}) => {
             <h1 className="landing-header">Welcome, lets organise your day!</h1>
 
             <form className="landing-form-container" onSubmit={handleSubmit}>
-                <input type='text' placeholder="Enter Name..." className="landing-input"></input>
-                <input type='text' placeholder="Enter Day" className="landing-input"></input>
+                <input type='text' placeholder="Enter Name..." className="landing-input" onChange={handleNameUpdate}></input>
+                <input type='text' placeholder="Enter Day" className="landing-input" onChange={handleDayUpdate}></input>
                 <button className="landing-btn">Get started!</button>
             </form>
         </div>
