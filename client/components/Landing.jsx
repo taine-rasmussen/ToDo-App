742480693f9dafdeafeeb69a46f0eddac527183a
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-04 15:58:58 +1300

    added handleSubmit func

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 
 const Landing = () => {
+
+
+
+    // Handle form submitting
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        e.target.reset()
+    }
+
     return(
         <div clasName="landing-container">
             <h1 className="landing-header">Welcome, lets organise your day!</h1>
 
-            <form className="landing-form-container">
+            <form className="landing-form-container" onSubmit={handleSubmit}>
                 <input type='text' placeholder="Enter Name..." className="landing-input"></input>
                 <input type='text' placeholder="Enter Day" className="landing-input"></input>
                 <button className="landing-btn">Get started!</button>
