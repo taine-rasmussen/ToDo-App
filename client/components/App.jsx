f08175a4996ee362f34ad7ae020ac52990437599
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-05 17:56:18 +1300

    refactored app removing landing and conditional rendering of main / landing component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react'
-import { Route } from 'react-router-dom'
+
 
 // Components
-import Landing from './Landing'
 import Main from './Main'
 
 
 function App () {
 
-  // State used to track input on landing / Display on main
-  const [name, setName] = useState('')
-
   return (
     <>
       <div className='app'>
-        <Route exact path='/' component={Landing} setName={setName} name={name}/>
-        <Route exact path='/list' component={Main } name={name}/>
+        <Main />
       </div>
     </>
   )
