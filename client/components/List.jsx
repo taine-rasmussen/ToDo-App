05d285d6a19913adf5ab7fb8cd4a30f432fc8832
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-05 14:44:11 +1300

    updated index.js to add react router

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -5,14 +5,27 @@ const List = () => {
 
     const [todo, setTodo] = useState([['Smile']])
 
+
+    const updateToDoList = (e) => {
+        e.preventDefault();
+        todo.push(e.target.value)
+    }
+
+    const checkBox = <input type="checkbox"></input>
+
     return(
         <div className="list-container">
             <div className="list-check-container"></div>
                 <div className="list-text-container">
-                    {todo.forEach(item => {
-                       return <h1>{item}</h1>
-                    })}
+                   {todo.map(item => {
+                       return <h2>{item}</h2>
+                   })}
                 </div>
+
+                    <form>
+                        <input type='text' placeholder='Enter task'></input>
+                        <button type='submit'>Add to list</button>
+                   </form>
         </div>
     )
 }
