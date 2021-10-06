6d982010bf6bdd55206842bb7a4fca6baea9d18d
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-06 20:25:25 +1300

    added handleSubmit / handleChange funcs to update list from input

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -3,27 +3,41 @@ import React, { useState } from 'react';
 
 const List = () => {
 
-    const [todo, setTodo] = useState([{task: 'smile'}])
+    // State used to track from input
+    const [input, setInput] = useState('')
 
-
-    const updateToDoList = (e) => {
-        e.preventDefault();
-        todo.push(e.target.value)
+    // State used to track task on todo list
+    const [task, setTask] = useState([[]])
+    
+    // Updates input state with form input
+    const handleChange = (e) => {
+        e.preventDefault()
+        setInput(e.target.value)
     }
 
-    const checkBox = <input type="checkbox"></input>
+    // Pushes form input into task state to be displayed as item on todo list
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        task.push(input)
+        setInput('')
+    }
 
     return(
         <div className="list-container">
             <div className="list-check-container"></div>
                 <div className="list-text-container">
-                   {todo.map(item => {
-                       return <h2>{item.task}</h2>
+                   {task.map(item => {
+                       return <h2>{item}</h2>
                    })}
                 </div>
 
-                    <form>
-                        <input type='text' placeholder='Enter task'></input>
+                    <form onSubmit={handleSubmit}>
+                        <input 
+                            type='text'
+                            placeholder='Enter task' 
+                            value={input} 
+                            onChange={handleChange}>
+                        </input>
                         <button type='submit'>Add to list</button>
                    </form>
         </div>
