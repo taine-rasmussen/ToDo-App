d623ac2e3010979f656c5be8c809b7c66c63ca2c
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-07 01:36:04 +1300

    added little feature that changes placeholder text after first item added to list

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -11,27 +11,44 @@ const List = () => {
 
     // State used to track task on todo list
     const [task, setTask] = useState([[]])
-    
-    // Updates input state with form input
-    const handleChange = (e) => {
-        e.preventDefault()
-        setInput(e.target.value)
-    }
 
-    // Pushes form input into task state to be displayed as item on todo list
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        task.push(input)
-        setInput('')
-    }
+    // State used to flip placeholder text after first item added
+    const [placeHolderChange, setPlaceHolderChange] = useState(true)
+        
+        // Flips placeholder state to display second message
+        const changePlaceHolder = () => {
+            setPlaceHolderChange(false)
+        }
 
+        // Variables used to display placeHolder text
+        const placeHolderOne = 'Add your first task...'
+        const placeHolderTwo = 'Add another task..'
+        
+        // Updates input state with form input
+        const handleChange = (e) => {
+            e.preventDefault()
+            setInput(e.target.value)
+        }
+
+        // Pushes form input into task state to be displayed as item on todo list
+        const handleSubmit = (e) => {
+            e.preventDefault()
+            task.push(input)
+            setInput('')
+            // Changes placeholder text after first input added to list
+            if (input.length > 1) {
+                changePlaceHolder()
+            } else {
+                null
+            }
+        }
 
     return(
         <div className="list-container">
             <form onSubmit={handleSubmit} className="form-container">
                 <input 
                     type='text'
-                    placeholder='Enter task...' 
+                    placeholder={placeHolderChange ? placeHolderOne : placeHolderTwo} 
                     value={input} 
                     onChange={handleChange}
                     className='form-input'>
